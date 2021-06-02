252eca64c64446447bae47164a01b44cef1a7069
NTDung05 <82139771+NTDung05@users.noreply.github.com>
2021-06-02 13:01:00 +0700

    2/6/2021 commit2

--- Booking/app/src/main/java/com/example/booking/BookingActivity.java
+++ Booking/app/src/main/java/com/example/booking/BookingActivity.java
@@ -2,14 +2,20 @@ package com.example.booking;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -20,19 +26,26 @@ import com.example.booking.Model.Price;
 import com.example.booking.Model.Room;
 import com.example.booking.Model.Room_type;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class BookingActivity extends AppCompatActivity {
-
+    private int lastSelectedYear = 2021;
+    private int lastSelectedMonth=6;
+    private int lastSelectedDayOfMonth=10;
     Button btnSearch;
     EditText edngaydat, edngaytra;
     Spinner spTypeRoom;
-    ListView lvRoom ;
+    ListView lvRoom;
     RoomAdapter customAdapter;
-private List<Room> room ;
-    String type[] = {"Tất cả","Phòng Đơn","Phòng Đôi", "Phòng Master"} ;
-    int count=0 ; //lấy loai phong cua spinner đẻ gán cho listview
+    final Calendar myCalendar = Calendar.getInstance();
+    private List<Room> room;
+    String type[] = {"Tất cả", "Phòng Đơn", "Phòng Đôi", "Phòng Master"};
+    int count = 0; //lấy loai phong cua spinner đẻ gán cho listview
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,48 +53,52 @@ private List<Room> room ;
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Booking");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
         Room_type t1 = new Room_type(1, 1);  //Phòng đơn
         Room_type t2 = new Room_type(2, 2); //Phòng đôi
         Room_type t3 = new Room_type(3, 3); // Phòng Mster
-        Price p1 = new Price(t1,1, 5000);
-        Price p2 = new Price(t3 ,0, 2000);
+        Price p1 = new Price(t1, 1, 5000);
+        Price p2 = new Price(t3, 0, 2000);
         Room r = new Room("101", t1, "View Biến",
-                "https://cms-assets.tutsplus.com/uploads/users/21/posts/19431/featured_image/CodeFeature.jpg",1);
+                "https://cms-assets.tutsplus.com/uploads/users/21/posts/19431/featured_image/CodeFeature.jpg", 1);
         Room r1 = new Room("102", t1, "View Biến",
-                "https://www.italianbark.com/wp-content/uploads/2018/01/hotel-room-design-trends-italianbark-interior-design-blog.jpg",1);
+                "https://www.italianbark.com/wp-content/uploads/2018/01/hotel-room-design-trends-italianbark-interior-design-blog.jpg", 1);
         Room r2 = new Room("103", t3, "View Núi",
-                "https://static01.nyt.com/images/2019/03/24/travel/24trending-shophotels1/24trending-shophotels1-superJumbo.jpg",1);
+                "https://static01.nyt.com/images/2019/03/24/travel/24trending-shophotels1/24trending-shophotels1-superJumbo.jpg", 1);
         Room r3 = new Room("104", t2, "View Biến",
-                "https://imgur.com/a/Lz5ibqc",1);
-     room =new ArrayList<>();
-         room.add(r);
+                "https://imgur.com/a/Lz5ibqc", 1);
+        room = new ArrayList<>();
+        room.add(r);
         room.add(r1);
         room.add(r2);
         room.add(r3);
         List<Room> room2 = new ArrayList<>();
         List<Room> room1 = new ArrayList<>();
         List<Room> room3 = new ArrayList<>();
-         for(int i = 0; i < room.size();i++){
-             if(room.get(i).getType().getNumber_of_bed()==1){
-                 room1.add(room.get(i));
-             }
-             if(room.get(i).getType().getNumber_of_bed()==2){
-                 room2.add(room.get(i));
-             }
-             if(room.get(i).getType().getNumber_of_bed()==3){
-                 room3.add(room.get(i));
-             }
-         }
+        for (int i = 0; i < room.size(); i++) {
+            if (room.get(i).getType().getNumber_of_bed() == 1) {
+                room1.add(room.get(i));
+            }
+            if (room.get(i).getType().getNumber_of_bed() == 2) {
+                room2.add(room.get(i));
+            }
+            if (room.get(i).getType().getNumber_of_bed() == 3) {
+                room3.add(room.get(i));
+            }
+        }
         setContentView(R.layout.activity_booking);
 
         setControl();
         setAdapterSpinner();
-        if(count == 0){
-        setAdapter(room);}
+        if (count == 0) {
+            setAdapter(room);
+        }
 
 
         setEvent(room1, room2, room3);
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -93,103 +110,180 @@ private List<Room> room ;
         }
         return super.onOptionsItemSelected(item);
     }
-    private void setControl(){
+
+    private void setControl() {
         btnSearch = (Button) findViewById(R.id.btnSearch);
-        spTypeRoom  = (Spinner) findViewById(R.id.spTypeRoom);
+        spTypeRoom = (Spinner) findViewById(R.id.spTypeRoom);
         lvRoom = (ListView) findViewById(R.id.lvRoom);
-        edngaydat = (EditText)findViewById(R.id.edNgaydat);
-        edngaytra = (EditText)findViewById(R.id.edNgaytra);
+        edngaydat = (EditText) findViewById(R.id.edNgaydat);
+        edngaytra = (EditText) findViewById(R.id.edNgaytra);
     }
 
-    public void setEvent(List<Room> room1, List<Room> room2,List<Room> room3){
-   btnSearch.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View v) {
 
-       }
-   });
-   
 
-   spTypeRoom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-       @Override
-       public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-          count = position;
-          System.out.print("hihihihihihihihih"+count);
-            if(count ==1){ setAdapter(room1);
-                lvRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                         Room test1 = room1.get(position);
-                        Intent intent = new Intent(getApplicationContext(), RoomDetailActivity.class);
-                        Bundle bundle = new Bundle();
-                        bundle.putSerializable("test1", test1);
-                        intent.putExtras(bundle);
-                        startActivity(intent);
-                    }
-                });
-            }
-           else if(count == 2) {setAdapter(room2);
-                lvRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Room test1 = room2.get(position);
-                        Intent intent = new Intent(getApplicationContext(),RoomDetailActivity.class);
-                        Bundle bundle = new Bundle();
-                        bundle.putSerializable("test1", test1);
-                        intent.putExtras(bundle);
-                        startActivity(intent);
-                    }
-                });}
-           else if(count == 3) {setAdapter(room3);
-                lvRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Room test1 = room3.get(position);
-                        Intent intent = new Intent(getApplicationContext(),RoomDetailActivity.class);
-                        Bundle bundle = new Bundle();
-                        bundle.putSerializable("test1", test1);
-                        intent.putExtras(bundle);
-                        startActivity(intent);
-                    }
-                });}
-           else {setAdapter(room);
-                lvRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Room test1 = room.get(position);
-                        Intent intent = new Intent(getApplicationContext(),RoomDetailActivity.class);
-                        Bundle bundle = new Bundle();
-                        bundle.putSerializable("test1", test1);
-                        intent.putExtras(bundle);
-                        startActivity(intent);
-                    }
-                });
+    public void setEvent(List<Room> room1, List<Room> room2, List<Room> room3) {
+
+       edngaydat.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+                   @Override
+                   public void onDateSet(DatePicker view, int year,
+                                         int monthOfYear, int dayOfMonth) {
+
+                       edngaydat.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                       lastSelectedYear = year;
+                       lastSelectedMonth = monthOfYear;
+                       lastSelectedDayOfMonth = dayOfMonth;
+                   }
+               };
+
+               DatePickerDialog datePickerDialog = null;
+               datePickerDialog = new DatePickerDialog(BookingActivity.this,
+                       dateSetListener, lastSelectedYear, lastSelectedMonth, lastSelectedDayOfMonth);
+               datePickerDialog.show();
+
            }
-       }
+       });
+       edngaytra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
 
-       @Override
-       public void onNothingSelected(AdapterView<?> parent) {
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
 
-       }
-   });
+                        edngaydat.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        lastSelectedYear = year;
+                        lastSelectedMonth = monthOfYear;
+                        lastSelectedDayOfMonth = dayOfMonth;
+                    }
+                };
+
+                DatePickerDialog datePickerDialog = null;
+                datePickerDialog = new DatePickerDialog(BookingActivity.this,
+                        dateSetListener, lastSelectedYear, lastSelectedMonth, lastSelectedDayOfMonth);
+                datePickerDialog.show();
+
+            }
+        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        edngaydat.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        lastSelectedYear = year;
+                        lastSelectedMonth = monthOfYear;
+                        lastSelectedDayOfMonth = dayOfMonth;
+                    }
+                };
+
+                DatePickerDialog datePickerDialog = null;
+                datePickerDialog = new DatePickerDialog(BookingActivity.this,
+                        dateSetListener, lastSelectedYear, lastSelectedMonth, lastSelectedDayOfMonth);
+                datePickerDialog.show();
+            }
+        });
+
+
+
+
+        spTypeRoom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                count = position;
+                System.out.print("hihihihihihihihih" + count);
+                if (count == 1) {
+                    setAdapter(room1);
+                    lvRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Room test1 = room1.get(position);
+                            Intent intent = new Intent(getApplicationContext(), RoomDetailActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putSerializable("test1", test1);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+                        }
+                    });
+                } else if (count == 2) {
+                    setAdapter(room2);
+                    lvRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Room test1 = room2.get(position);
+                            Intent intent = new Intent(getApplicationContext(), RoomDetailActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putSerializable("test1", test1);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+                        }
+                    });
+                } else if (count == 3) {
+                    setAdapter(room3);
+                    lvRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Room test1 = room3.get(position);
+                            Intent intent = new Intent(getApplicationContext(), RoomDetailActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putSerializable("test1", test1);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+                        }
+                    });
+                } else {
+                    setAdapter(room);
+                    lvRoom.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Room test1 = room.get(position);
+                            Intent intent = new Intent(getApplicationContext(), RoomDetailActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putSerializable("test1", test1);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
     }
 
 
     private void setAdapter(List<Room> r) {
-       // (customAdapter == null) {
-            customAdapter = new RoomAdapter(this, R.layout.custom_list_view_room, r);
-            lvRoom.setAdapter(customAdapter);
+        // (customAdapter == null) {
+        customAdapter = new RoomAdapter(this, R.layout.custom_list_view_room, r);
+        lvRoom.setAdapter(customAdapter);
 
-      //  } else {
+        //  } else {
         //    customAdapter.notifyDataSetChanged();
-          //  lvRoom.setSelection(customAdapter.getCount() - 1);
+        //  lvRoom.setSelection(customAdapter.getCount() - 1);
         //}
     }
+
     private void setAdapterSpinner() {
         ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, type);
         adapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
         spTypeRoom.setAdapter(adapter);
     }
+
 }
+
