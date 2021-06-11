fb2a6fe7072d6c09f3dfb74d18ab3d0bc99459b3
NTDung05 <82139771+NTDung05@users.noreply.github.com>
2021-06-12 04:28:14 +0700

    continue 12/6

--- Booking/app/src/main/java/com/example/booking/PieChartActivity.java
+++ Booking/app/src/main/java/com/example/booking/PieChartActivity.java
@@ -2,9 +2,12 @@ package com.example.booking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import com.example.booking.Api.ApiService;
+import com.example.booking.Model.Month;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -13,16 +16,30 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-public class PieChartActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class PieChartActivity extends AppCompatActivity {
+    PieChart pieChart;
+    ArrayList<PieEntry> pieEntries;
+    String year=" ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pie_chart);
 
-        PieChart pieChart = findViewById(R.id.pieChart);
-        ArrayList<PieEntry> pieEntries = new ArrayList<>();
+         pieChart =(PieChart) findViewById(R.id.pieChart);
+        pieEntries = new ArrayList<>();
+        Intent myintent = getIntent();
+        Bundle bundle = myintent.getExtras();
+        year = bundle.getString("year");
+        int t = Integer.parseInt(year);
+        DoanhThu(t);
 
+
+    }
+    public void set(){
         pieEntries.add(new PieEntry(10000000, "Phong xin vl"));
         pieEntries.add(new PieEntry(20000000, "Phong dom vl"));
         pieEntries.add(new PieEntry(30000000, "Phong cui vl"));
@@ -38,5 +55,19 @@ public class PieChartActivity extends AppCompatActivity {
         pieChart.getDescription().setEnabled(false);
         pieChart.setCenterText("Doanh Thu Theo Loại Phòng");
         pieChart.animate();
+    }
+    private void DoanhThu(int y){
+        ApiService.API_SERVICE.GetChart(y).enqueue(new Callback<Month>() {
+            @Override
+            public void onResponse(Call<Month> call, Response<Month> response) {
+           //     month = response.body();
+                set();
+            }
+
+            @Override
+            public void onFailure(Call<Month> call, Throwable t) {
+
+            }
+        });
     }
 }
